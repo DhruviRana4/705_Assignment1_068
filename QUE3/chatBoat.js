eec3fab4d26e8f6e2e68e1e4dd428cb3e47b0b8b
unknown <dhruvirana.mscit20@vnsgu.ac.in>
2023-07-27 03:00:04 +0530

    QUE3

--- QUE3/chatBoat.js
+++ QUE3/chatBoat.js
@@ -4,7 +4,28 @@ module.exports.reply = function (msg) {
 
     msg = msg.toLowerCase();
 
-    if(msg.indexOf("hi")>-1){
-        return "Hello..!!";
-    }
+    if(msg.indexOf("hi") > -1 || 
+			msg.indexOf("hello") > -1 || 
+			msg.indexOf("welcome") > -1 )
+		{
+			return "Hi!";
+		} 
+		else if(msg.indexOf("age") > -1 && 
+			msg.indexOf("your"))
+		{
+			return "I'm " + this.Bot_Age;
+		}
+		else if (msg.indexOf("how") > -1 && 
+			msg.indexOf("are") && 
+			msg.indexOf("you"))
+		{
+			return "I'm fine ^_^"
+		}
+		else if(msg.indexOf("where") > -1 
+			&& msg.indexOf("live") && 
+			msg.indexOf("you"))
+		{
+			return "I live in " + this.Bot_Country;
+		}
+		return "Sorry, I didn't get it :( ";
 }
