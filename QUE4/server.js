d12f3d62da0f3d7b9fce9c0ac10133cebfc3d901
DhruviRana <dhruvirana987@gmail.com>
2023-07-30 11:46:23 +0530

    QUE4 done

---
+++ QUE4/server.js
@@ -0,0 +1,27 @@
+const http=require("http");
+const { Http2ServerRequest } = require("http2");
+const static=require("node-static");
+const url=require("url");
+const websocket=require("ws");             
+
+var fileServer=new static.Server("./public");
+var server=http.createServer((req,res)=>{
+    var url2=url.parse(req.url,true);
+    // console.log("Hello NodeJs...!!");
+    if(url2.pathname==""){
+        console.log(url2)
+    }
+    fileServer.serve(req,res);
+})
+
+server.listen(8000,()=>{
+    console.log("server listening on port 8000");
+})
+
+var wss=new websocket.Server({server:server});
+wss.on("connection",(ws)=>{
+    ws.send("hello client..!!");
+    ws.on("message",(msg)=>{
+        ws.send("I recieved ==> " + msg);
+    })
+})
